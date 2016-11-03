ddfddeff8f7e76472a54f7055f3d005f4a7d69e4
Paul Hoang <p3hoang@gmail.com>
2016-11-03 11:43:31 -0700

    fixed home page characters

---
+++ public/js/login.js
@@ -0,0 +1,15 @@
+function pullJSON(){
+			
+	$.ajax ({
+	    url: '/json/login.json',
+	    dataType: 'text',
+	    cache: false,
+	    success: function(data) {
+	        var obj = JSON.parse(data);
+	        console.log(data);
+			document.getElementById("uname").value = obj.username;
+			document.getElementById("password").value = obj.password;
+			window.location = "/";
+	    }
+	});
+}

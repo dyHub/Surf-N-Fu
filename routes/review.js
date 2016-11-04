5632a3300761bca1760ebe9cad9d02f4ce22f991
yuhan <yud039@ucsd.edu>
2016-11-03 20:15:22 -0700

    added json file

--- routes/review.js
+++ routes/review.js
@@ -3,14 +3,26 @@
  */
 
 exports.view = function(req, res){
-  var activity = req.params.activity;
-  var mapLink = '/map/' + activity;
-  var weatherLink = "/weather/" + activity;
+    var activity = req.params.activity;
+    var mapLink = '/map/' + activity;
+    var weatherLink = "/weather/" + activity;
 
-  console.log("activity is " + activity);
-  res.render('review', {
-  	'activity': activity,
-  	'weatherLink': weatherLink,
-    'mapLink': mapLink
-  });
+    var beachJson = require("../public/json/beaches.json");
+
+    console.log("activity is " + activity);
+    var beach = req.params.beach;
+    if(beach){
+        res.render('review_beach', {
+            'activity': activity,
+            'weatherLink': weatherLink,
+            'mapLink': mapLink,
+            'beachName': beachJson["beaches"][beach]
+        });
+    } else {
+        res.render('review', {
+            'activity': activity,
+            'weatherLink': weatherLink,
+            'mapLink': mapLink
+        });
+    }
 };

5db1efb14d3db7a6ae400705d893ccd6864dad70
yuhan dai <yud039@ucsd.edu>
2016-11-06 17:30:26 -0800

    weather timeline template

--- routes/weather.js
+++ routes/weather.js
@@ -8,13 +8,17 @@ exports.view = function(req, res){
     var weatherLink = "/weather/" + activity;
     var reviewLink = '/review/' + activity;
     var beachJson = require("../public/json/beaches.json");
+    var beaches = beachJson['beaches'];
 
     console.log("activity is " + activity);
+    console.log("beaches " + beaches);
+
     res.render('weather', {
         'activity': activity,
         'mapLink': mapLink,
         'weatherLink': weatherLink,
         'reviewLink': reviewLink,
+        'beaches': beaches,
         'isWeather': true
     });
 };

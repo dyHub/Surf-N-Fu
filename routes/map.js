bde73484b03bf60a7735d7c92f5018ecfc3f65a2
yuhan dai <yud039@ucsd.edu>
2016-11-10 23:09:19 -0800

    render the pop out

--- routes/map.js
+++ routes/map.js
@@ -7,22 +7,43 @@ exports.view = function(req, res){
     var mapLink = '/map/' + activity;
     var weatherLink = "/weather/" + activity;
     var reviewLink = '/review/' + activity;
-    var image = "/images/" + activity + ".png";
     var googleKey = process.env.GOOGLEKEY;
     var googleAPI = "https://maps.googleapis.com/maps/api/js?key=" + googleKey + "&callback=initMap"
     var beachJson = require("../public/json/beachesArr.json");
     //console.log(beachJson);
 
+    var request = require("request");
+    var weatherKey = process.env.WUKEYBACKUP;
+
     console.log("activity is " + activity);
-    res.render('map', {
-        'activity': activity,
-        'mapLink': mapLink,
-        'weatherLink': weatherLink,
-        'reviewLink': reviewLink,
-        'image': image,
-        'googleAPI': googleAPI,
-        'isMap': true,
-        'arr': beachJson['beaches']
+    var thisBeach = beachJson['beaches'][0];
+    var weatherUrl = "http://api.wunderground.com/api/" + weatherKey + "/conditions/q/" + thisBeach['latitude'] + "," + thisBeach['longitude'] +".json";
+
+    request({
+        url: weatherUrl,
+        json: true
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body); // Print the json response
+            var weatherData = body['current_observation'];
+
+            res.render('map', {
+                'activity': activity,
+                'mapLink': mapLink,
+                'weatherLink': weatherLink,
+                'reviewLink': reviewLink,
+                'googleAPI': googleAPI,
+                'weatherData': weatherData,
+                'isMap': true,
+                'arr': beachJson['beaches']
+            });
+        } else {
+            console.log(error);
+        }
     });
+
+
+
+
 };
 
